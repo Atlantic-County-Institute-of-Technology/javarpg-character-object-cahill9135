dfd42fbc7bf0e0031f6d7dde575fbe9f2c897f67
cahill9135 <126170328+cahill9135@users.noreply.github.com>
2026-01-30 12:55:39 -0500

    my dice file
    its a little different and i dont want any errrors from the main.

--- Dice.java
+++ Dice.java
@@ -1,111 +1,139 @@
-// Dice.java
-// @description: A class that handles multiple dice of any number of sides using an arraylist
-// @author: pcostjr
-// created: 1.7.2026
-// last update: 1.20.2026
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Random;
 
-// a class for managing multiple dice with any number of sides.
-// dice are stored persistently and can be rolled individually or together.
-public class Dice {
+class Dice {
+
     // instance variables
     private ArrayList<Die> dice;
-    
-    // creates an empty collection of dice
+
+    // creates an empty collection for the dice
     public Dice() {
         this.dice = new ArrayList<>();
     }
 
-    // adds a single die with sides
-    public int addDie(int sides) {
-        if (sides < 1) {
-            throw new IllegalArgumentException("Die must have at least 1 side");
-        }
-        dice.add(new Die(sides));
-        return dice.size() - 1;
+    public void addDie(int sides) {
+        if(sides > 0)
+            dice.add(new Die(sides));
     }
 
-    // adds multiple dice with the specified number of sides to the arraylist
-    public void addDice(int count, int sides) {
-        if (count < 0) {
-            throw new IllegalArgumentException("Count cannot be negative");
-        }
-        for (int i = 0; i < count; i++) {
+    //adds a number of dice equal to count (>0) dice to any of the sides (>0) to the list
+    public void addDie(int count, int sides) {
+        for(int i=0; i <= count; i++)
             addDie(sides);
-        }
     }
 
-    // rolls a specific die at the target index, if it exists.
+    //rolls a die at a specific index, returns -1 if no dice to be rolled
     public int rollDie(int index) {
-        if (index < 0 || index >= dice.size()) {
+        if (!dice.isEmpty()) {
+            return dice.get(index).roll();
+        }
+        else{
+            return -1;
+        }
+    };
+
+    //rolls a set of dice between two indexes
+    public int rollDice(int start, int end){
+        if (!dice.isEmpty()) {
+            int result = 0;
+            for(int i = start; i < end; i++){
+                result += dice.get(i).roll();
+            }
+            return result;
+        }
+        else {
+            return -1;
+        }
+    };
+
+    //rolls all dice in the list
+    public int rollAll(){
+        if (!dice.isEmpty()) {
+            int result = 0;
+            for(int i = 0; i < dice.size(); i++){
+                result += dice.get(i).roll();
+            }
+            return result;
+        }
+        else {
+            return -1;
+        }
+    };
+
+    //Returns the last rolled value of a given index
+    public int getDieValue(int index){
+        if (!dice.isEmpty()) {
+            return dice.get(index).getValue();
+        }
+        else{
+            return -1;
+        }
+    };
+
+    //Returns all the values of the last rolled dice, added together
+    public int getAllValues(){
+        if (!dice.isEmpty()) {
+            int result = 0;
+            for(int i = 0; i < dice.size(); i++){
+                result += dice.get(i).getValue();
+            }
+            return result;
+        }
+        else {
+            return -1;
+        }
+    };
+
+    public void removeDie(int index) {
+        if (index < 0 || index >= dice.size()){
             throw new IndexOutOfBoundsException("Invalid die index: " + index);
         }
-        return dice.get(index).roll();
-    }
-    
-
-    // rolls all dice and returns the sum total of the values
-    public int rollAll() {
-        int sum = 0;
-        for (Die die : dice) {
-            sum += die.roll();
-        }
-        return sum;
+        dice.remove(index);
     }
 
-    // gets the current value of a specific die without rolling it
-    // might not be needed but I had this already
-    public int getDieValue(int index) {
-        if (index < 0 || index >= dice.size()) {
-            throw new IndexOutOfBoundsException("Invalid die index: " + index);
-        }
-        return dice.get(index).getValue();
+    public void sortDice() {
+        Collections.sort(dice, new Comparator<Die>() {
+            public int compare(Die d1, Die d2) {
+                return Integer.compare(d1.getValue(), d2.getValue());
+            }
+        });
+
     }
-    
-    // returns the sum total of dice without rolling them
-    public int getAllValues() {
-        int sum = 0;
-        for (int i = 0; i < dice.size(); i++) {
-            sum+= dice.get(i).getValue();
-        }
-        return sum;
-    }
-    
-    // returns the size of the arraylist
+
+    //Return the size of the list
     public int size(){
         return dice.size();
-    }
-    
-    // clears all dice from the collection
-    public void clear() {
-        dice.clear();
-    }
+    };
 
-    // inner class will only be used by dice, so we can embed it here
+    //Clears the dice list
+    public void clear(){
+        dice.clear();
+
+    };
+
+    // The die object class used by our dice
     public static class Die implements DieInterface {
+
         private int sides;
         private int currentValue;
         private Random random;
-        
+
         public Die(int sides) {
             this.sides = sides;
             this.random = new Random();
-            this.currentValue = 1; // Initial value
+            this.currentValue = 1; // Initial Value
         }
 
         public int roll() {
-            return currentValue = random.nextInt(sides) + 1;
-        }
-
-        public int getValue() {
+            currentValue = random.nextInt(sides) + 1;
             return currentValue;
         }
 
-        public int getSides() {
-            return sides;
-        }
+        public int getValue(){ return currentValue;};
+
+        public int getSides() {return sides;}
+
     }
 }
