55552f4380091afaa721789fedaaf6d543e3bc3e
cahill9135 <126170328+cahill9135@users.noreply.github.com>
2026-01-30 12:56:17 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+// Imports!!! Yay!!!!
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Character whitheredjohn = new Character();
+        System.out.println(whitheredjohn.rollStat());
+    }
+}
