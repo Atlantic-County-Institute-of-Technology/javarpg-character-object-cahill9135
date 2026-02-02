a367672deaf418cbf039701656fb6ff0b03c8dfa
cahill9135 <126170328+cahill9135@users.noreply.github.com>
2026-02-01 19:26:52 -0500

    Update Character.java
    somethings broken and i cant figure it out so im gonna ask mister c on mondayu

--- Character.java
+++ Character.java
@@ -16,10 +16,12 @@ public class Character {
     public int armorClass;
     public int characterLevel;
     public String charClass;
+    public String name;
 
     Dice dice = new Dice();
 
     public void createCharacter(String cName, String cClass){
+        name = cName;
         characterMatcher = new HashMap<>();
 
         for (int i=0; i<8; i++) {
@@ -38,24 +40,58 @@ public class Character {
 
     }
 
+
+
+    public int getPrimaryAbilityModifier(){
+        return switch (charClass) {
+            case "Melee" -> getAbilityModifier(statLine[0]);
+            case "Ranged" -> getAbilityModifier(statLine[1]);
+            case "Magic" -> getAbilityModifier(statLine[2]);
+            default -> 0;
+        };
+    };
+
     public int rollStat() {
         dice.clear();
         dice.addDie(4, 6);
         dice.rollAll();
         dice.sortDice();
-        dice.removeDie(3);
+        dice.removeDie(0);
         return dice.getAllValues();
     }
 
     public void rollStats() {
         dice.clear();
         dice.addDie(4, 6);
-        for(int i = 0; i <= 5; i++){
-            dice.rollDice(0, 3);
+        for (int i = 0; i <= 5; i++) {
+            dice.rollAll();
             dice.sortDice();
-
+            dice.removeDie(0);
+            statLine[i] = dice.getAllValues();
+            dice.addDie(6);
         }
+
+        for (int i = 0; i <=5; i++){
+            statBonus[i] = getAbilityModifier(statLine[i]);
+        }
+        calculateBaseHealth();
+
+    getStats();
     }
+
+    public int getAbilityModifier(int abilityScore) {
+        return (abilityScore - 10)/2;
+    };
+
+    public int calculateBaseHealth() {
+        hitpoints = 20 + getAbilityModifier(statLine[3]);
+        return hitpoints;
+    };
+
+    public int calculateArmorClass() {
+        armorClass = 10 + getPrimaryAbilityModifier();
+        return armorClass;
+    };
 
     public void getStats() {
         String result = "";
@@ -80,4 +116,27 @@ public class Character {
     public int getHitpoints() {
         return hitpoints;
     }
+
+    public boolean isAlive() {
+        return hitpoints > 0;
+    };
+
+    public String toString() {
+        String result = "Character: ";
+        result += name + " (Level " + characterLevel + " " + charClass + ")\n"
+        + "HP: " + calculateBaseHealth() + " | AC: " + calculateArmorClass() + "\n";
+
+        for (int i = 0; i <=2; i++ ){
+            result += statLabel[i] + ": " + statLine[i] + " + " + statBonus[i] + " | ";
+        }
+
+        result += "\n";
+
+        for (int i = 3; i <=5; i++ ){
+            result += statLabel[i] + ": " + statLine[i] + " + " + statBonus[i] + " | ";
+        }
+
+        return result;
+    };
+
 }
