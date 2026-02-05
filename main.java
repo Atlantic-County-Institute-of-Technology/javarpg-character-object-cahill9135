ace9f094f943e1763ab3cb8db0e0ef48fe687783
cahill9135 <126170328+cahill9135@users.noreply.github.com>
2026-02-05 12:48:40 -0500

    Update main.java

--- main.java
+++ main.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Character whitheredjohn = new Character();
-        System.out.println(whitheredjohn.rollStat());
+        whitheredjohn.createCharacter("john", "ranger");
+        whitheredjohn.rollStats();
+        System.out.println(whitheredjohn.isAlive());
+        System.out.println(whitheredjohn);
+        System.out.println(whitheredjohn.getArmorClass());
+        System.out.println(whitheredjohn.getCharClass());
+        System.out.println(whitheredjohn.getHitpoints());
     }
 }
